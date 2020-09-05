7c92e8853a16221bfbc17d8f97fc383af99d9105
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2020-09-05 16:15:59 +0100

    add user login and JWT signing

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const mongoose = require("mongoose")
 const User = require("../models/user")
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 
 router.post('/signup', (req, res, next) => {
@@ -36,6 +37,41 @@ router.post('/signup', (req, res, next) => {
                     }
                 })
             }
+        })
+})
+
+router.post('/login', (req, res, next)=>{
+    User.find({email: req.body.email})
+        .then(user =>{
+            if(user.length < 1){
+                return res.status(401).json({
+                    message: 'Login failed'
+                })
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result)=>{
+                if(err || !result){
+                    return res.status(401).json({
+                        message: 'Login failed'
+                    })
+                }
+                const token = jwt.sign({
+                    email: user[0].email,
+                    userId: user[0]._id
+                }, "secret",
+                {
+                    expiresIn: "1h"
+                })
+                return res.status(200).json({
+                    message: 'Auth successful',
+                    token: token
+                })
+            })
+        })
+        .catch(err =>{
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
         })
 })
 
